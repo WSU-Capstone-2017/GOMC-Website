1020e351419901f841f53a8b14d7393419c90038
muamer <m_besic@yahoo.com>
2017-09-27 17:21:28 -0700

    Added function 'public static T....' in 'HomeControllersTest.cs' as well as created 2 different test methods in 'HomeControllersTest.cs'

--- Tests/Controllers/HomeControllersTest.cs
+++ Tests/Controllers/HomeControllersTest.cs
@@ -1,5 +1,7 @@
-﻿
+﻿using Project.Models;
+using Project.Core;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
 
 namespace Project.Controllers
 {
@@ -9,9 +11,25 @@ namespace Project.Controllers
         [TestMethod]
         public void GetDownloadModelNullTest()
         {
-            var homeController = new HomeController();
-            var model = homeController.GetDownloadModel();
+            var hc = new HomeController();
+            var model = hc.GetDownloadModel();
             Assert.IsNotNull(model);
+        }
+        [TestMethod]
+        public void DownloadsActionDoesNotReturnNullModel()
+        {
+            var hc = new HomeController();
+            var actionResult = hc.Downloads();           
+            var model2 = Utils.ModelFromActionResult<DownloadsModel>(actionResult);
+            Assert.IsNotNull(model2);
+        }      
+        [TestMethod]
+        public void ContactActionDoesNotReturnNullModel()
+        {
+            var hc = new HomeController();
+            var actionResult = hc.Contact();
+            var model3 = Utils.ModelFromActionResult<HomeModel>(actionResult);
+            Assert.IsNotNull(model3);
         }
     }
 }
