077f3ebf13dae73c032c72f5bd4ad734c1dc2ac8
Caleb Latimer <nohitokaze@gmail.com>
2017-10-08 16:17:28 -0400

    Pure.css -> Bootstrap migration complete
    Changed frameworks, installed dependencies, configured existing code to fit framework choice. Completed Navigation bar, Completed Footer. Added in color scheme from client, added font choice from client, added in menu options from client

---
+++ Prototypes/prototype-1-2/gulpfile.js
@@ -0,0 +1,29 @@
+// Prototype-1-2- gulp file
+// Automate tasks for P-1-2
+// Variables
+var gulp = require('gulp')
+
+// Concatenate Javascript
+gulp.task('concat',function(){
+
+});
+
+// Minimize css
+gulp.task('minimizeCSS', function(){
+
+});
+
+// change less -> CSS
+gulp.task('lessToCSS', function(){
+
+});
+
+// Lint Javascript
+gulp.task('lint', function(){
+
+});
+
+//  Run main automation suite
+gulp.task('default', function(){
+
+});
