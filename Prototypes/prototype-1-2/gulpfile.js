2b3a8b8a02ea73de4e801e5ae2724966dc9d45bc
Caleb <nohitokaze@gmail.com>
2017-10-09 11:14:09 -0400

    About.html is complete
    I still need to tweak the styles, but all the content is there. Now to beat it into shape. Starting Resources.html

--- Prototypes/prototype-1-2/gulpfile.js
+++ Prototypes/prototype-1-2/gulpfile.js
@@ -1,6 +1,25 @@
 // Prototype-1-2- gulp file
 // Automate tasks for P-1-2
 // Variables
+
+
+// imported file
+var gulp = require('gulp')
+var gulp = require('gulp');
+var less = require('gulp-less');
+var watch = require('gulp-watch');
+
+
+gulp.task('lessToCss', function() {
+  return gulp.src('styles/*.less')
+    .pipe(watch('styles/*.less'))
+    .pipe(less())
+    .pipe(gulp.dest('styles/updated.css'));
+});
+
+// end import
+
+
 var gulp = require('gulp')
 
 // Concatenate Javascript
