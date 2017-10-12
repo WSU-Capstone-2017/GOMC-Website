147fe89958021ac480f5b5af46adda1d330786fd
Caleb <nohitokaze@gmail.com>
2017-10-12 19:49:12 -0400

    Admin page added and features for front-end started
    I am checking in current UI changes, a new page and the new forms that are currently under development. The next goal is to get these flushed out and connected to Ahmed so he can continue. Class is over cwfh

--- Prototypes/prototype-1-2/javascript/gomc.js
+++ Prototypes/prototype-1-2/javascript/gomc.js
@@ -20,3 +20,25 @@ $('#btn').click(function(){
        $('header').css('margin-top', '6.5em');
     }
 });
+
+$('#xmlTrigger').click(function(){
+  $('#xmlDataContainer').slideToggle(700,function(){
+    $('#xmlDataContainer').toggleClass('hidden-until');
+    $('#xmlTrigger').slideUp(500, 'linear', morphXmlTrigger);
+  });
+});
+
+// Callback methods
+function morphXmlTrigger(){
+  if($('#xmlDataContainer').hasClass('hidden-until')){
+    $('#xmlTrigger').removeClass('btn-danger');
+    $('#xmlTrigger').addClass('btn-primary');
+    $('#xmlTrigger').html('Start');
+  }
+  else{
+    $('#xmlTrigger').removeClass('btn-primary');
+    $('#xmlTrigger').addClass('btn-danger');
+    $('#xmlTrigger').html('<span class="glyphicon glyphicon-remove"></span>');
+  }
+  $(this).slideDown();
+}
