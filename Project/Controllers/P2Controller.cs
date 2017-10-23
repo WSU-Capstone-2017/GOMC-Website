38b36a759e4dd36b15eaab59e9d2ae8710a3a8a3
Ahmed Taher <ataher2012@gmail.com>
2017-10-22 20:51:27 -0400

    Added download model to download view

--- Project/Controllers/P2Controller.cs
+++ Project/Controllers/P2Controller.cs
@@ -1,9 +1,33 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Project.Core;
+using Project.Models;
 
 namespace Project.Controllers
 {
 	public class P2Controller : Controller
 	{
+		public DownloadsModel GetDownloadModel()
+		{
+			var rsp = Utils.SimpleGet("https://api.github.com/repos/GOMC-WSU/GOMC/releases");
+			var jsn = Newtonsoft.Json.Linq.JArray.Parse(rsp);
+
+			dynamic jsn0 = jsn[0];
+			string tag = jsn0.tag_name;
+
+			dynamic assets = jsn0.assets;
+
+
+			var items = new List<DownloadsModel.DownloadItem>();
+			foreach (dynamic i in assets)
+			{
+				string name = i.name;
+				string iurl = i.browser_download_url;
+
+				items.Add(new DownloadsModel.DownloadItem(name, iurl));
+			}
+			return new DownloadsModel(null, tag, items);
+		}
 		public ActionResult Gomc()
 		{
 			return View();
@@ -14,7 +38,7 @@ namespace Project.Controllers
 		}
 		public ActionResult Downloads()
 		{
-			return View();
+			return View(GetDownloadModel());
 		}
 		public ActionResult Documentation()
 		{
