a66a40bde21800832bf45abba3664078e49ca717
Ahmed Taher <ataher2012@gmail.com>
2017-10-26 11:52:39 -0400

    Fixed build issues related to untracked files, removed un-needed code

--- Project/App_Start/BundleConfig.cs
+++ Project/App_Start/BundleConfig.cs
@@ -26,8 +26,7 @@ namespace Project
 				"~/Scripts/gomc.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include(
-					  "~/Content/bootstrap.css",
-					  "~/Content/site.css"));
+					  "~/Content/bootstrap.css"));
 
 			bundles.Add(
 				new StyleBundle("~/Content/bootstrap").Include(
