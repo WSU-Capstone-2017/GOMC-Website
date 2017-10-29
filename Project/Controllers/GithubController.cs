57dafc214fa4583196fe619ffd4eaf9653a2a873
Ahmed Taher <ataher2012@gmail.com>
2017-10-29 08:54:37 -0400

    Created callback for github api webhooks, added github controller to accept callback

---
+++ Project/Controllers/GithubController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using Newtonsoft.Json.Linq;
+
+namespace Project.Controllers
+{
+	public class GithubController : ApiController
+	{
+		private static readonly Dictionary<string,string> formMap = new Dictionary<string, string>();
+
+		[HttpPost]
+		public IHttpActionResult Callback(JObject jobj)
+		{
+			formMap.Clear();
+			foreach(var j in jobj.Properties())
+			{
+				var	val = j.Value.ToString();
+				formMap.Add(j.Name, val);
+			}
+			return Ok(formMap);
+		}
+		[HttpGet]
+		public string LastCallback()
+		{
+			return formMap.Select(j => $"{j.Key}:{j.Value}\n").Aggregate("", (a, b) => a += b);
+		}
+	}
+
+}
