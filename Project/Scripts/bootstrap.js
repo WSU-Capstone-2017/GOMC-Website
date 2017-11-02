411c6f1b4a0eac7e8a1789ca4235e9f3d665767b
muamer <m_besic@yahoo.com>
2017-11-02 03:06:47 -0700

    Added hover color to core.less, JS for collapse function (+,-), deleted emails/access ID's, made faq into a list with collapsible view, changed site map to have glyphs (opened,closed) and looks better

--- Project/Scripts/bootstrap.js
+++ Project/Scripts/bootstrap.js
@@ -2375,3 +2375,10 @@ if (typeof jQuery === 'undefined') {
   })
 
 }(jQuery);
+
+$('.collapse').on('shown.bs.collapse', function(){
+$(this).parent().find(".glyphicon-plus").removeClass("glyphicon-plus").addClass("glyphicon-minus");
+}).on('hidden.bs.collapse', function(){
+$(this).parent().find(".glyphicon-minus").removeClass("glyphicon-minus").addClass("glyphicon-plus");
+});
+
