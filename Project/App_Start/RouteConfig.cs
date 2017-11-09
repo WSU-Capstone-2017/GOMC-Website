bb02b794cd409b5edf1259075d7facf5268e2fce
Ahmed Taher <ataher2012@gmail.com>
2017-11-08 19:44:27 -0500

    Wired front-end to the new announcements back-end api, added new fetch announcements api

--- Project/App_Start/RouteConfig.cs
+++ Project/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace Project
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Gomc", id = UrlParameter.Optional }
+				defaults: new { controller = "home", action = "gomc", id = UrlParameter.Optional }
 			);
 		}
 	}
