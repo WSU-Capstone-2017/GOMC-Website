e447828addebd0c851304428f69bc89e79a1c083
Ahmed Taher <ataher2012@gmail.com>
2017-11-10 14:18:50 -0500

    Improved github web-hooks callback

--- Project/Controllers/GithubController.cs
+++ Project/Controllers/GithubController.cs
@@ -14,54 +14,95 @@ namespace Project.Controllers
 {
 	public class GithubController : ApiController
 	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
 		[HttpPost]
-		public IHttpActionResult Callback([FromBody] PingModel ping)
+		public IHttpActionResult Callback(JObject input)
 		{
-			return Ok(ping);
-		}
-
-		[HttpPost]
-		public IHttpActionResult Callback([FromBody] PushModel push)
-		{
-			foreach (var c in push.Commits)
+			try
 			{
-				if (c.Modified.Contains("Manual.tex"))
+				log.Info("Github Webhooks--");
+
+				var githubEvents = Request.Headers.GetValues("X-GitHub-Event").ToList();
+				if (githubEvents.Contains("ping"))
 				{
-					var rsp = Utils.SimpleGet(
-						"https://api.github.com/repos/ataher1992/GOMC_Manual/contents/Manual.tex");
-					var jsn = JObject.Parse(rsp);
-					var durl = jsn["download_url"];
-					string file;
-					using (var wc = new WebClient())
+					var ping = input.ToObject<PingModel>();
+
+					log.Info("Ping Event");
+					return Ok(ping);
+				}
+
+				if (!githubEvents.Contains("push"))
+				{
+					log.Error("Unrecognized input");
+					return Ok(input);
+
+				}
+				var push = input.ToObject<PushModel>();
+
+
+				log.Info("Push Event");
+				log.Info(push.Ref);
+
+				foreach (var c in push.Commits)
+				{
+					if (c.Modified.Contains("Manual.tex"))
 					{
-						file = wc.DownloadString(durl.ToString());
-					}
-					if (UploadLatex(file, push.HeadCommit.Id, push.Pusher.Email))
-					{
-						return Ok("Manual updated.");
-					}
-					else
-					{
-						return Ok("Manual did not update.");
+						log.Info("Manual.tex was changed");
+
+						var rsp = Utils.SimpleGet(
+							"https://api.github.com/repos/ataher1992/GOMC_Manual/contents/Manual.tex");
+
+						log.Info("Getting Manual.tex from repo");
+
+						var jsn = JObject.Parse(rsp);
+						var durl = jsn["download_url"];
+
+						string file;
+						using (var wc = new WebClient())
+						{
+							file = wc.DownloadString(durl.ToString());
+						}
+
+						log.Info("Uploading..");
+						log.Info(push.HeadCommit.Id);
+						log.Info(push.Pusher.Email);
+						if (UploadLatex(file, push.HeadCommit.Id, push.Pusher.Email))
+						{
+							log.Info("Uploaded succeed");
+							return Ok("Manual updated.");
+						}
+						else
+						{
+							log.Warn("Upload failed");
+							return Ok("Manual did not update.");
+						}
 					}
 				}
+				return Ok("Manual.tex was not changed");
 			}
-			return Ok("Manual.tex was not changed");
+			catch(Exception e)
+			{
+				log.Error(e.ToString());
+				return InternalServerError(e);
+			}
 		}
 
 		private static bool UploadLatex(string file, string version, string email)
 		{
 			using (var db = new ProjectDbContext())
 			{
+				log.Info($"fetching email '{email}'");
+
 				var prm = new SqlParameter("@inputEmail", email);
 
 				var s = db.UserLogins.SqlQuery(
 					"SELECT * FROM dbo.UserLoginModels WHERE Email = @inputEmail", prm)
 					.SingleOrDefault();
 
-				if(s == null)
+				if (s == null)
 				{
+					log.Warn("email not found");
 					return false;
 				}
 
