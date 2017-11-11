2560a153fe80b02c3db3c5864ba1c0f29b718dff
Ahmed Taher <ataher2012@gmail.com>
2017-11-11 12:58:47 -0500

    Switched the latex html convertor to htlatex

--- Project/Latex/LatexConvertor.cs
+++ Project/Latex/LatexConvertor.cs
@@ -2,8 +2,10 @@
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Web;
 
 namespace Project.Latex
@@ -11,6 +13,9 @@ namespace Project.Latex
 	public class LatexConvertor
 	{
 		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+		[DllImport("User32.Dll", EntryPoint = "PostMessageA")]
+		private static extern bool PostMessage(IntPtr hWnd, uint msg, int wParam, int lParam);
 
 		public byte[] HtmlZip { get; set; }
 		public byte[] Pdf { get; set; }
@@ -38,6 +43,7 @@ namespace Project.Latex
 
 			var fileName = Path.Combine(fileDir, "Manual.tex").Replace("\\", "/");
 			var latexFileContent = File.ReadAllText(fileName);
+
 			// the current version of the latex file has images referenced with out specifying the extension
 			// htlatex is sensitive about that and so we must provide the extension. all the extensions are
 			// .png except images/website which is .jpg
@@ -49,26 +55,38 @@ namespace Project.Latex
 
 			try
 			{
+				log.Info("running htlatex...");
 
 				var htmlProc = new Process
 				{
-					StartInfo = new ProcessStartInfo("pandoc", "-o Manual.html Manual.tex")
+					StartInfo = new ProcessStartInfo("htlatex", "Manual.tex")
 					{
-						WorkingDirectory = fileDir,
-						WindowStyle = ProcessWindowStyle.Hidden
+						WorkingDirectory = fileDir
 					}
 				};
 
-				log.Info("running pandoc...");
 
 				htmlProc.Start();
-				htmlProc.WaitForExit();
 
-				log.Info("pandoc done");
+				while (!htmlProc.HasExited)
+				{
+					// htlatex will request user input when there is latex errors,
+					// we will press enter each second to simulate user input
+					// TODO: find an alternative way to run htlatex without using the PostMessage hack
+					Thread.Sleep(1000);
+					PostMessage(htmlProc.MainWindowHandle, 0x100, 0x0D, 0);
+				}
 
-				using(var zip = ZipFile.Open(Path.Combine(fileDir, "html.zip"), ZipArchiveMode.Create))
+				log.Info("htlatex done");
+
+				using (var zip = ZipFile.Open(Path.Combine(fileDir, "html.zip"), ZipArchiveMode.Create))
 				{
 					zip.CreateEntryFromFile(Path.Combine(fileDir, "Manual.Html"), "Manual.html");
+					zip.CreateEntryFromFile(Path.Combine(fileDir, "Manual.css"), "Manual.html");
+					foreach (var i in new DirectoryInfo(fileDir).GetFiles("Manual*x.png"))
+					{
+						zip.CreateEntryFromFile(i.FullName, i.Name);
+					}
 				}
 
 				HtmlZip = File.ReadAllBytes(Path.Combine(fileDir, "html.zip"));
@@ -99,7 +117,7 @@ namespace Project.Latex
 
 				Pdf = File.ReadAllBytes(Path.Combine(fileDir, "Manual.pdf"));
 			}
-			catch(Exception ex)
+			catch (Exception ex)
 			{
 				log.Error(ex.ToString(), ex);
 				return ConversionResult.Invalid;
