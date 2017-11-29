d58b4e190741b7faa94a748ff125f2d53d772882
muamer <m_besic@yahoo.com>
2017-11-28 19:35:25 -0800

    Unit Test for valid password/email

--- Tests/Controller/LogincControllerTests.cs
+++ Tests/Controller/LogincControllerTests.cs
@@ -12,5 +12,30 @@ namespace Project.Controller
 	[TestClass]
 	public class LogincControllerTests
 	{
+        [TestMethod]
+        public void ValidEmailAndPassword()
+        {
+            var mk = new ProjectDbContext.MockType
+            {
+                UserLogins = new[]
+                {
+                    new UserLoginModel
+                    {
+                        Email = "test@email.com", PasswordHash = LoginManager.GetHash("password")
+                    }
+                }
+            };
+
+            var lm = new LoginController(() => ProjectDbContext.Mock(mk));
+            var map = new Dictionary<string, string>
+            {
+                {"uName", "test@email.com" },
+                {"pCode", "password" }
+            };
+
+            var form = new FormDataCollection(map);
+            var ret = lm.ValidateLogin(form);
+            Debug.Assert(ret?.ResultType == LoginResultType.Success);
+        }
 	}
 }
