266e03c4abda689386b99e5d78707e7c87505357
Ahmed Taher <ataher2012@gmail.com>
2017-12-05 20:49:19 -0500

    added sql files

---
+++ Project/SQL/Registrations.sql
@@ -0,0 +1,41 @@
+﻿/*    ==Scripting Parameters==
+
+    Source Server Version : SQL Server 2016 (13.0.4422)
+    Source Database Engine Edition : Microsoft SQL Server Express Edition
+    Source Database Engine Type : Standalone SQL Server
+
+    Target Server Version : SQL Server 2016
+    Target Database Engine Edition : Microsoft SQL Server Express Edition
+    Target Database Engine Type : Standalone SQL Server
+*/
+
+USE [projectdb]
+GO
+
+/****** Object:  Table [dbo].[Registrations]    Script Date: 12/5/2017 8:47:11 PM ******/
+DROP TABLE [dbo].[Registrations]
+GO
+
+/****** Object:  Table [dbo].[Registrations]    Script Date: 12/5/2017 8:47:11 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Registrations](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](max) NULL,
+	[Email] [nvarchar](max) NULL,
+	[Affiliation] [nvarchar](max) NULL,
+	[Title] [nvarchar](max) NULL,
+	[Text] [nvarchar](max) NULL,
+	[Created] [datetime] NULL,
+ CONSTRAINT [PK_dbo.Registrations] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+
+
